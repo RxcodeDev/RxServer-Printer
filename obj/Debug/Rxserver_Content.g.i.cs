fbdb3d102152c6d32f0beb04d1303bc4e9c51daa
PM <rxcode.dev@gmail.com>
2025-06-12 03:20:17 -0600

    Correccion a la impresion de ticket

--- obj/Debug/Rxserver_Content.g.i.cs
+++ obj/Debug/Rxserver_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/raapidinlogogray.bmp")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("app.ico")]
 
 
